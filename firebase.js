fe55b04fc03c3544b3d949d064d66716e253b14e
himanipilankar18 <himanipilankar@gmail.com>
2025-04-19 22:09:35 +0530

    after adding welcome and some chnage sin homepage App.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,27 @@
-// firebase/firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // Firestore for database
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA0f5LDRWSBEjByEp8ficZA7YJF7lW9Op8",
   authDomain: "khana-khajana-f8e17.firebaseapp.com",
   projectId: "khana-khajana-f8e17",
-  storageBucket: "khana-khajana-f8e17.appspot.com", // fixed typo here!
+  storageBucket: "khana-khajana-f8e17.firebasestorage.app",
   messagingSenderId: "452733800675",
-  appId: "1:452733800675:web:9e7fbf35bb43a3edbdb5d6"
+  appId: "1:452733800675:web:9e7fbf35bb43a3edbdb5d6",
+  measurementId: "G-4BMS70GD8K"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // You’ll use this to read/write vendor data
 
-export { db };
+// Get Firestore instance
+const db = getFirestore(app);
+
+// Export Firestore and utilities
+export { db, collection, getDocs };
+
